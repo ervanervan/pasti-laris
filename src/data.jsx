c0652237741c9f1cbf89d6d94bc6e8252712c3a9
ervanervan <ervanervan040@gmail.com>
2024-03-27 14:45:40 +0700

    update: logo partners

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,35 @@
+import logo1 from "./assets/logo-1.png";
+import logo2 from "./assets/logo-2.png";
+import logo3 from "./assets/logo-3.png";
+import logo4 from "./assets/logo-4.png";
+import logo5 from "./assets/logo-5.png";
+
 export const DATA_NAVBARS = [
   { id: 1, title: "Features", link: "#features" },
   { id: 2, title: "vs. Slock", link: "#vsslock" },
   { id: 3, title: "Pricing", link: "#pricing" },
   { id: 4, title: "Showcase", link: "#showcase" },
+];
+
+export const DATA_PARTNERS = [
+  {
+    id: 1,
+    imgURL: logo1,
+  },
+  {
+    id: 2,
+    imgURL: logo2,
+  },
+  {
+    id: 3,
+    imgURL: logo3,
+  },
+  {
+    id: 4,
+    imgURL: logo4,
+  },
+  {
+    id: 5,
+    imgURL: logo5,
+  },
 ];
