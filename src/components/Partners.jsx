7505af23f096f7756fa47cef2f1a164f91abd0f3
ervanervan <ervanervan040@gmail.com>
2024-03-27 15:08:10 +0700

    add partners and update hero

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { DATA_PARTNERS } from "../data";
 
 export default function Partners() {
   return (
-    <div className="">
-      <h2 className="text-3xl font-bold text-center">
+    <div className="flex flex-col gap-y-8 p-4">
+      <h2 className="text-3xl font-bold text-center leading-[2.75rem]">
         We’re Helping Big <br /> Global Companies Grow
       </h2>
+      <div className="flex flex-wrap items-center justify-around md:justify-center gap-5 lg:gap-16">
+        {DATA_PARTNERS.map((item) => (
+          <img key={item.id} src={item.imgURL} alt="" className="w-32" />
+        ))}
+      </div>
     </div>
   );
 }
