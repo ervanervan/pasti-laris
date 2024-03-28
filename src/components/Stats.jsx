77e43f5c4555a938e62aa264bb6805037fe6b53c
ervanervan <ervanervan040@gmail.com>
2024-03-28 14:09:19 +0700

    update: finish stats left section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import vector1 from "../assets/vector1.svg";
 
 export default function Stats() {
-  return <div>Stats</div>;
+  return (
+    <section className="bg-green-1">
+      <div className="container mx-auto relative z-0">
+        <div className="flex items-center justify-between px-4 py-8 md:py-16 gap-8 relative z-10">
+          <div>
+            <h2 className="text-white-1 font-bold text-3xl leading-[3rem]">
+              We Have Big Reasons <br /> Improve People’s Life Today
+            </h2>
+          </div>
+          <div className="">items 2</div>
+        </div>
+        <img
+          src={vector1}
+          alt=""
+          className="absolute top-0 left-36 h-full -z-10"
+        />
+      </div>
+    </section>
+  );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Stats from "../components/Stats";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Stats />
     </>
   );
 };
