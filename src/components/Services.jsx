fd55c68e213905097c837e3988431149f7ae164a
ervanervan <ervanervan040@gmail.com>
2024-03-29 20:42:41 +0700

    update: service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Services() {
   return (
-    <div>
-      <h1>Good For Your Business</h1>
-      <p>We have designed a feature based on latest research</p>
-    </div>
+    <section className="container mx-auto">
+      <div className="text-center py-24">
+        <h1 className="font-extrabold text-5xl">Good For Your Business</h1>
+        <p>We have designed a feature based on latest research</p>
+      </div>
+    </section>
   );
 }
