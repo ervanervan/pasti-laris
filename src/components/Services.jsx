3cc6397a78f273a99ed4a1333937a196241b9498
ervanervan <ervanervan040@gmail.com>
2024-03-30 18:28:32 +0700

    add: react-tabs

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+
+import iconCrown from "../assets/icon-crown.svg";
 
 export default function Services() {
   return (
-    <section className="container mx-auto">
-      <div className="text-center py-24">
-        <h1 className="font-extrabold text-5xl">Good For Your Business</h1>
-        <p>We have designed a feature based on latest research</p>
+    <section className="">
+      <div className="py-24 container mx-auto">
+        <div className="text-center">
+          <h1 className="font-extrabold text-5xl leading-[4rem]">
+            Good For Your Business
+          </h1>
+          <p>We have designed a feature based on latest research</p>
+        </div>
       </div>
+      <Tabs className="flex flex-col items-center">
+        <TabList className="flex items-centers gap-10">
+          <Tab className="flex items-center gap-4 cursor-pointer">
+            <div className="p-3 rounded-full bg-green-2">
+              <img src={iconCrown} alt="" />
+            </div>
+            <span className="font-semibold">Automations</span>
+          </Tab>
+          <Tab className="flex items-center gap-4 cursor-pointer">
+            <div className="p-3 rounded-full bg-green-2">
+              <img src={iconCrown} alt="" />
+            </div>
+            <span>Automations</span>
+          </Tab>
+        </TabList>
+        <TabPanel>1111111111111</TabPanel>
+        <TabPanel>2222222222222</TabPanel>
+      </Tabs>
     </section>
   );
 }
