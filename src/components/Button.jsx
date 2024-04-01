8857239e04ba338a1d2ee11251dcf55275b3d8b6
ervanervan <ervanervan040@gmail.com>
2024-04-01 08:32:43 +0700

    update: font size button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Button = ({ children, onClick, variant, size }) => {
-  let buttonClass = "rounded-full font-semibold";
+  let buttonClass = "rounded-full";
 
   // Set variant styles
   if (variant === "primary") {
-    buttonClass += " px-8 py-5 bg-green-2 text-green-1";
+    buttonClass += " px-8 py-5 bg-green-2 text-green-1 font-bold";
   } else if (variant === "secondary") {
     buttonClass +=
-      " px-8 py-5 bg-tranparent text-green-1 border border-green-1";
+      " px-8 py-5 bg-tranparent text-green-1 border border-green-1 font-semibold";
   } else if (variant === "white") {
-    buttonClass += " px-5 py-3 bg-white-1 text-green-1";
+    buttonClass += " px-5 py-3 bg-white-1 text-green-1 font-semibold";
   } else if (variant === "white border") {
     buttonClass +=
-      " px-5 py-3 bg-tranparent text-white-1 border border-white-1";
+      " px-5 py-3 bg-tranparent text-white-1 border border-white-1 font-semibold";
   }
 
   // Set size styles
