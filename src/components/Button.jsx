43885814db000934d2148caf1d2597156e89c1b3
ervanervan <ervanervan040@gmail.com>
2024-04-15 19:09:22 +0700

    update: button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,10 @@ const Button = ({ children, onClick, variant, size }) => {
 
   // Set variant styles
   if (variant === "primary") {
-    buttonClass += " px-8 py-5 bg-green-2 text-green-1 font-bold";
+    buttonClass += " px-8 py-4 md:py-5 bg-green-2 text-green-1 font-bold";
   } else if (variant === "secondary") {
     buttonClass +=
-      " px-8 py-5 bg-tranparent text-green-1 border border-green-1 font-semibold";
+      " px-8 py-4 md:py-5 bg-tranparent text-green-1 border border-green-1 font-semibold";
   } else if (variant === "white") {
     buttonClass += " px-5 py-3 bg-white-1 text-green-1 font-semibold";
   } else if (variant === "white border") {
